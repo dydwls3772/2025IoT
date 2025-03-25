43e1cc964e87695ad5235a93231bbda0776e8c10
dydwls3772 <mh781121@naver.com>
2025-03-25 19:50:23 +0900

    Update week04-01.ino

--- source/week04-01.ino
+++ source/week04-01.ino
@@ -4,17 +4,8 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-int Vo = A0;
-int V_led = 7;
-
-float Vo_value = 0;
-float Voltage = 0;
-float dustDensity = 0;
-
 void setup()
 {
-  pinMode(V_led, OUTPUT);
-  pinMode(Vo, INPUT);
   lcd.init();					// I2C LCD 초기화
   lcd.backlight();				// 백라이트 켜기
   lcd.print("LCD init");
@@ -24,20 +15,11 @@ void setup()
 
 void loop()
 {
-  digitalWrite(V_led, LOW);
-  delayMicroseconds(280);
-  Vo_value = analogRead(Vo);
-  delayMicroseconds(40);
-  digitalWrite(V_led, HIGH);
-  delayMicroseconds(9680);
-
-  Voltage = Vo_value*5.0 / 1023.0;
-  dustDensity = (Voltage - 0.5)/0.005;
   lcd.setCursor(0,0);
-
-  lcd.print(dustDensity);
-  lcd.print("um");       
-  delay (150);
-  lcd.clear();
-
+  lcd.print("Hello, World!");
+            
+  for (int position = 0; position < 16; position++){
+    lcd.scrollDisplayLeft();
+    delay (150);
+  }
 }
